ba9797fb30b708d3f24aee17b75f65c85c1a9e5a
Seokmin <ghdtjrald240@gmail.com>
2024-05-12 23:59:03 +0900

    Add a test

---
+++ examples/nodejs-ts/src/no-compare-neg-zero.test.ts
@@ -0,0 +1,15 @@
+import eslint from './utils/eslint';
+
+eslint('no-compare-neg-zero', [
+  {
+    ruleId: 'no-compare-neg-zero',
+    severity: 2,
+    message: "Do not use the '===' operator to compare against -0.",
+    line: 3,
+    column: 5,
+    nodeType: 'BinaryExpression',
+    messageId: 'unexpected',
+    endLine: 3,
+    endColumn: 13,
+  },
+]);
---
+++ examples/nodejs-ts/src/no-compare-neg-zero.ts
@@ -0,0 +1,5 @@
+const x = 0 as number;
+
+if (x === -0) {
+  console.log(x);
+}

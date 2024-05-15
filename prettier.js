a0cd29eb3233135c0eb2209ad4071065e8ecedbd
Seokmin <ghdtjrald240@gmail.com>
2024-05-15 15:07:57 +0900

    Add Prettier config

---
+++ prettier.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/**
+ * @type {import('prettier').Config}
+ */
+export default {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
